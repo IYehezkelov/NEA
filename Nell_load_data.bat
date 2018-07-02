5403a01fc01435e513c58a343906a7b1787b0200
IYehezkelov <itay.yehezkelov@gmail.com>
2018-07-02 12:30:19 +0300

    ..

--- Nell_load_data.bat
+++ Nell_load_data.bat
@@ -4,7 +4,7 @@
 ::                                                                            ::
 ::  1. Settings & General definition                                          ::
 ::                                                                            ::
-::	2. EPMAutomate commands																										::
+::  2. EPMAutomate commands                                                   ::
 ::    2.1 Login                                                               ::
 ::    2.2 Clear old file from inbox                                           ::
 ::    2.3 Load new file to inbox                                              ::
